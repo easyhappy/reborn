3de08bd7c95fca9e4f7ae8ed63b9f87cdb2731fc
AndyHu <meeasyhappy@gmail.com>
2014-06-23 17:40:26 +0800

    新建rails 项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reborn::Application.config.secret_token = '72e7052fdfc64a41e805be14b553649a4bde1b3ccb56f706443933cf526e5533079391bc5657a5a50ec9f77b69704aa043873de1514b935d50acd674c62207fe'
