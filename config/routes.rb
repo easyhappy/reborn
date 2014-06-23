ecd0f2d94d2bbef4cb440bc8dffce0fb3def04e1
andyHu <meeasyhappy@gmail.com>
2014-06-23 22:07:50 +0800

    添加背景图片

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Reborn::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  root to: 'home#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -14,7 +14,7 @@ Reborn::Application.routes.draw do
   #   resources :products
 
   # Sample resource route with options:
-  #   resources :products do
+  #   resources :products do:
   #     member do
   #       get 'short'
   #       post 'toggle'
