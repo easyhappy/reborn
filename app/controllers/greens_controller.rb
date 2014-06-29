c587cb7cb1266597e292f00849ec86d127218f55
andyHu <meeasyhappy@gmail.com>
2014-06-29 22:25:45 +0800

    添加green的前端展示

--- app/controllers/greens_controller.rb
+++ app/controllers/greens_controller.rb
@@ -1,4 +1,6 @@
 class GreensController < ApplicationController
   def index
+    @green_types = GreenType.all
+    @greens = Green.where(:green_type_id => params[:type_id] ||@green_types.first.id)
   end
 end
